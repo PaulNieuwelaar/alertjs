f3c9df29c2c2f4cbf691b4821c9100d64e33316e
Paul Nieuwelaar <PaulNieuwelaar@users.noreply.github.com>
2016-07-26 05:23:09 +0000

    v2 - made it faster, more supported, smaller, and just generally better!

--- AlertJS/AlertJS.Web/mag_/js/alert.js
+++ AlertJS/AlertJS.Web/mag_/js/alert.js
@@ -13,96 +13,14 @@ Alert.show("Would you like to create a new Sale?", null,
 
 var Alert = Alert || {};
 
+Alert._prefix = "mag_"; // Change this if you have your own solution prefix (as long as the file structure's the same)
 Alert._dialogHeight = 225;
 Alert._dialogWidth = 450;
 
-Alert._callbackFunctions = [];
+Alert._initialised = false;
+Alert._context = null;
 
-Alert._callback = function (value) {
-    if (value !== null && value !== undefined) {
-        for (var i = 0; i < Alert._callbackFunctions.length; i++) {
-            if (Alert._callbackFunctions[i].label == value) {
-                var callback = Alert._callbackFunctions[i].callback;
-                if (callback) {
-                    callback();
-                    break;
-                }
-            }
-        }
-    }
-}
-
-// purpose: open a webpage in a CRM lightbox (web) or modal dialog (outlook)
-// Works well for CRM dialog processes, web resources, or any other alert style webpage
-Alert.tryShowDialog = function (url, width, height, callback, baseUrl) {
-    width = width || Alert._dialogWidth;
-    height = height || Alert._dialogHeight;
-
-    var isOpened = false;
-
-    try {
-        // Web (IE, Chrome, FireFox)
-        var Mscrm = Mscrm && Mscrm.CrmDialog && Mscrm.CrmUri && Mscrm.CrmUri.create ? Mscrm : parent.Mscrm;
-        if (Mscrm && Mscrm.CrmDialog && Mscrm.CrmUri && Mscrm.CrmUri.create) {
-            // Use CRM light-box (unsupported)
-            var crmUrl = Mscrm.CrmUri.create(url);
-            var dialogwindow = new Mscrm.CrmDialog(crmUrl, window, width, height);
-
-            // Allows for opening non-webresources (e.g. dialog processes) - CRM messes up when it's not a web resource (unsupported)
-            if (!crmUrl.get_isWebResource()) {
-                crmUrl.get_isWebResource = function () { return true; }
-            }
-
-            // Open the lightbox
-            dialogwindow.show();
-            isOpened = true;
-
-            // Make sure when the dialog is closed, the callback is fired
-            // This part's all pretty unsupported, hence the try-catches
-            if (callback) {
-                try {
-                    // Get the lightbox iframe (unsupported)
-                    var $frame = parent.$("#InlineDialog_Iframe");
-                    $frame.load(function () {
-                        try {
-                            // Override the CRM closeWindow function (unsupported)
-                            var frameDoc = $frame[0].contentWindow;
-                            var closeEvt = frameDoc.closeWindow; // OOTB close function
-                            frameDoc.closeWindow = function () {
-                                // Bypasses onunload event on dialogs to prevent "are you sure..." (unsupported)
-                                try { frameDoc.Mscrm.GlobalVars.$B = false; } catch (e) { }
-
-                                // Fire the callback and close
-                                callback();
-                                try { closeEvt(); } catch (e) { }
-                            }
-                        } catch (e) { }
-                    });
-                } catch (e) { }
-            }
-        }
-    } catch (e) { }
-
-    try {
-        // Outlook
-        if (!isOpened && window.showModalDialog) {
-            // If lightbox fails, use window.showModalDialog
-            baseUrl = baseUrl || Xrm.Page.context.getClientUrl();
-            var params = "dialogWidth:" + width + "px; dialogHeight:" + height + "px; status:no; scroll:no; help:no; resizable:yes";
-
-            window.showModalDialog(baseUrl + url, window, params);
-            if (callback) {
-                callback();
-            }
-
-            isOpened = true;
-        }
-    } catch (e) { }
-
-    return isOpened;
-}
-
-// purpose: display an alert style dialog for the user using the CRM lightbox (web) or modal dialog (outlook)
+// purpose: display an alert style dialog for the user using a styled CRM lightbox
 // Allows for custom buttons and callbacks
 // title = Main big message
 // message = (optional) Sub-heading shown below the title
@@ -111,79 +29,148 @@ Alert.tryShowDialog = function (url, width, height, callback, baseUrl) {
 // width = (optional, defaults to _dialogWidth) Custom width of the dialog
 // height = (optional, defaults to _dialogHeight) Custom height of the dialog
 // baseUrl = (optional, defaults to getClientUrl) Base url of CRM (only required if no access to Xrm.Page)
-Alert.show = function (title, message, buttons, icon, width, height, baseUrl) {
+// preventCancel = (optional, defaults to false) Hides the 'X' in the top right corner, meaning you can only dismiss the alert using the buttons
+Alert.show = function (title, message, buttons, icon, width, height, baseUrl, preventCancel) {
+    title = title || "";
+    message = message || "";
     width = width || Alert._dialogWidth;
     height = height || Alert._dialogHeight;
     buttons = buttons || [{ label: "OK" }];
-
-    Alert._callbackFunctions = buttons;
-
-    var queryStrings = "title=" + (title || "") + "&message=" + (message || "") + "&icon=" + (icon || "");
-    for (var i = 0; i < buttons.length; i++) {
-        queryStrings += "&buttons[]=" + buttons[i].label;
-    }
-
-    var url = "/webresources/mag_/html/alert_dialog.html?Data=" + encodeURIComponent(queryStrings);
-
-    // Our web resource handles the callback functions since they can be different for each button
-    if (Alert.tryShowDialog(url, width, height)) { return; }
-
-    // Future proofing...
     baseUrl = baseUrl || Xrm.Page.context.getClientUrl();
-    if (window.showModelessDialog) {
-        var params = "dialogWidth:" + width + "px; dialogHeight:" + height + "px; status:no; scroll:no; help:no; resizable:yes";
-        window.showModelessDialog(baseUrl + url, window, params);
-    }
-    else if (window.open) {
-        // If modal and modeless failed, use window.open since it opens a nice dialog
-        var left = (screen.width / 2) - (width / 2);
-        var top = (screen.height / 2) - (height / 2);
-        var params = "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" + width + ", height=" + height + ", top=" + top + ", left=" + left;
 
-        window.open(baseUrl + url, null, params);
+    if (!Alert._initialised) {
+        // The parent/top document which we append the wrapper to
+        Alert._context = window.top.document;
+
+        // The wrapper sits outside the form, so it may exist even if Alert.js is not initialised
+        var alertJsWrapper = $("#alertJs-wrapper", Alert._context);
+        if (alertJsWrapper == null || alertJsWrapper.length == 0) {
+            var alertJsHtml =
+                "<div id='alertJs-wrapper' class='alert-js-wrapper'>" +
+                  "<link rel='stylesheet' href='" + baseUrl + "/WebResources/" + Alert._prefix + "/css/alert.css' />" +
+                  "<div class='alert-js-background'></div>" +
+                  "<div id='alertJs-dialog' class='alert-js-dialog'>" +
+                    "<div id='alertJs-closeWrapper' class='alert-js-close-wrapper'>" +
+                      "<div id='alertJs-close' class='alert-js-close' title='Cancel'></div>" +
+                    "</div>" +
+                    "<div class='alert-js-RefreshDialog-Warning' id='alertJs-divWarning'>" +
+                      "<table class='alert-js-table-wrapper' cellspacing='0' cellpadding='0'>" +
+                        "<tr id='alertJs-errorRow' class='alert-js-ErrorMessage-row'>" +
+                          "<td id='alertJs-imageWrapper' class='alert-js-image-td'>" +
+                            "<div id='alertJs-image' class='alert-js-image'></div>" +
+                          "</td>" +
+                          "<td class='alert-js-error-td'>" +
+                            "<div class='alert-js-Error-Header ms-crm-Field-Data-Print' id='alertJs-title'></div>" +
+                            "<div class='alert-js-Error-Message ms-crm-Field-Data-Print' id='alertJs-message'></div>" +
+                          "</td>" +
+                        "</tr>" +
+                      "</table>" +
+                    "</div>" +
+                    "<div class='alert-js-RefreshDialog-Footer' id='alertJs-tdDialogFooter'></div>" +
+                  "</div>" +
+                "</div>";
+
+            $("body", Alert._context).append(alertJsHtml);
+        }
+
+        // Attach close event (messes up with jquery)
+        var closeButton = $("#alertJs-close", Alert._context);
+        if (closeButton && closeButton.length > 0) {
+            closeButton[0].onclick = function () {
+                Alert.hide();
+            };
+        }
+
+        Alert._initialised = true;
     }
-        // If the dialog couldn't open let's try a few more options
-    else if (Xrm && Xrm.Utility && Xrm.Utility.openWebResource) {
-        // If window.open doesn't work, try the supported openWebResource method which opens new tab
-        Xrm.Utility.openWebResource("mag_/html/alert_dialog.html", encodeURIComponent(queryStrings), width, height);
-    }
-    else if (buttons.length == 1) {
-        // If window.open fails, and there's only 1 button, display an alert
-        if (Xrm && Xrm.Utility && Xrm.Utility.alertDialog) {
-            Xrm.Utility.alertDialog(title, buttons[0].callback);
-        }
-        else {
-            alert(title);
-            if (buttons[0].callback) {
-                buttons[0].callback();
-            }
-        }
-    }
-    else if (buttons.length == 2) {
-        // If window.open fails, and there's 2 buttons, display a confirm (assumes the 'ok' button is on the left)
-        if (Xrm && Xrm.Utility && Xrm.Utility.confirmDialog) {
-            Xrm.Utility.confirmDialog(title, buttons[0].callback, buttons[1].callback);
-        }
-        else {
-            if (confirm(title)) {
-                if (buttons[0].callback) {
-                    buttons[0].callback();
-                }
-            }
-            else {
-                if (buttons[1].callback) {
-                    buttons[1].callback();
-                }
-            }
-        }
+
+    $("#alertJs-title", Alert._context).html(title);
+    $("#alertJs-message", Alert._context).html(message);
+
+    if (icon && ["INFO", "WARNING", "ERROR", "SUCCESS", "QUESTION"].indexOf(icon) !== -1) {
+        var imgType = icon == "ERROR" ? "crit" : icon == "WARNING" ? "warn" : icon == "INFO" ? "info" : icon == "SUCCESS" ? "tick" : "ques";
+
+        $("#alertJs-imageWrapper", Alert._context).removeClass("alert-js-hide");
+
+        // Remove any existing image classes before adding the new one
+        $("#alertJs-image", Alert._context)
+            .removeClass("alert-js-image-crit alert-js-image-warn alert-js-image-info alert-js-image-tick alert-js-image-ques")
+            .addClass("alert-js-image-" + imgType);;
     }
     else {
-        // If all else fails, display an error occurred message
-        if (Xrm && Xrm.Utility && Xrm.Utility.alertDialog) {
-            Xrm.Utility.alertDialog("An error has occurred while generating this message. Please contact your CRM Administrator.");
-        }
-        else {
-            alert("An error has occurred while generating this message. Please contact your CRM Administrator.");
-        }
+        $("#alertJs-imageWrapper", Alert._context).addClass("alert-js-hide");
     }
+
+    // Delete existing buttons
+    $("#alertJs-tdDialogFooter", Alert._context).empty();
+
+    // Create new buttons
+    for (var i = 0; i < buttons.length; i++) {
+        var $button = $("<button>", { tabindex: "1", type: "button" });
+        $button.addClass("alert-js-RefreshDialog-Button");
+        $button.html(buttons[i].label);
+
+        // Create the callback for the button
+        (function (callback, preventClose) {
+            $button.click(function () {
+                Alert._buttonClicked(callback, preventClose);
+            });
+        })(buttons[i].callback, buttons[i].preventClose);
+
+        $("#alertJs-tdDialogFooter", Alert._context).append($button);
+    }
+
+    // Show or hide the manual cancel button
+    if (preventCancel) { $("#alertJs-closeWrapper", Alert._context).hide(); }
+    else { $("#alertJs-closeWrapper", Alert._context).show(); }
+
+    // Makes the formatting nicer if the popup is huge (for displaying trace logs etc)
+    if (height > 350) { $("#alertJs-errorRow", Alert._context).addClass("alert-js-ErrorMessage-row-top"); }
+    else { $("#alertJs-errorRow", Alert._context).removeClass("alert-js-ErrorMessage-row-top"); }
+
+    // Set height/width and display
+    $("#alertJs-dialog", Alert._context).css("height", height).css("width", width).css("margin-top", height * -0.5).css("margin-left", width * -0.5);
+    $("#alertJs-message", Alert._context).css("max-height", height - 140);
+    $("#alertJs-wrapper", Alert._context).show();
+}
+
+// Hide the alert manually without performing any callbacks
+Alert.hide = function () {
+    if (Alert._initialised) {
+        $("#alertJs-wrapper", Alert._context).hide();
+    }
+}
+
+Alert._buttonClicked = function (callback, preventClose) {
+    $(".alert-js-RefreshDialog-Button", Alert._context).prop("disabled", true);
+
+    try {
+        // Calls the callback function
+        if (callback) {
+            callback();
+        }
+
+        if (!preventClose) {
+            Alert.hide();
+        }
+    } catch (e) {
+        alert(e);
+    }
+
+    $(".alert-js-RefreshDialog-Button", Alert._context).prop("disabled", false);
+}
+
+// Encode the Title or Message to display xml tags, e.g. from a plugin error trace
+// Also replaces javascript line breaks with <br>
+Alert.htmlEncode = function (s) {
+    if (s == null || typeof s !== "string") { return s; }
+
+    return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/  /g, '&nbsp;&nbsp;').replace(/\n/g, '<br />');
+}
+
+// Helper to build the buttons
+Alert.Button = function (label, callback, preventClose) {
+    this.label = label;
+    this.callback = callback;
+    this.preventClose = preventClose;
 }
