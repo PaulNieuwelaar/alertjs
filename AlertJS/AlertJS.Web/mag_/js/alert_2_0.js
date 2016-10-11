87cef6ad9cdb233cd55bbe05d7e5d4c99a1e61e5
Liger_cp <SND\Liger_cp>
2016-10-11 00:26:55 +0000

    v2.0.2 and v2.1

--- AlertJS/AlertJS.Web/mag_/js/alert_2_0.js
+++ AlertJS/AlertJS.Web/mag_/js/alert_2_0.js
@@ -1,6 +1,4 @@
-﻿// Alert.js v2.1 - Copyright Paul Nieuwelaar Magnetism 2016
-
-/*    
+﻿/*    
 Alert.show("Would you like to create a sale?", "This will create and open the new sale record.",
     [
         new Alert.Button("Create Sale", function () {
@@ -18,15 +16,8 @@ Alert._dialogWidth = 500;
 
 Alert._initialised = false;
 Alert._context = null;
-Alert._crmContext = null;
 
-// Stores the jQuery reference for the CRM main page (if turbo etc)
-Alert._jQuery = window.jQuery ? window.$ : parent.$;
-
-// Custom jQuery wrapper to use jquery from the parent CRM page to access elements from the top page where Alertjs is
-Alert.$ = function (selector, context) {
-    return Alert._jQuery(selector, context || Alert._context);
-}
+$ = window.jQuery ? window.$ : parent.$;
 
 // purpose: display an alert style dialog for the user using a styled CRM lightbox
 // Allows for custom buttons and callbacks
@@ -38,32 +29,29 @@ Alert.$ = function (selector, context) {
 // height = (optional, defaults to _dialogHeight) Custom height of the dialog
 // baseUrl = (optional, defaults to getClientUrl) Base url of CRM (only required if no access to Xrm.Page)
 // preventCancel = (optional, defaults to false) Hides the 'X' in the top right corner, meaning you can only dismiss the alert using the buttons
-// padding = (optional, defaults to 30) Sets the amount of padding around the light-box. Set to 0 for no padding (on iframes etc)
-Alert.show = function (title, message, buttons, icon, width, height, baseUrl, preventCancel, padding) {
+Alert.show = function (title, message, buttons, icon, width, height, baseUrl, preventCancel) {
     title = title || "";
     message = message || "";
     width = width || Alert._dialogWidth;
     height = height || Alert._dialogHeight;
     buttons = buttons || [{ label: "OK" }];
     baseUrl = baseUrl || Xrm.Page.context.getClientUrl();
-    if (padding === undefined || padding === null) { padding = 20; }
 
     if (!Alert._initialised) {
         // The parent/top document which we append the wrapper to
         Alert._context = window.top.document;
 
-        // The CRM window, for calling back from an Alert iframe. Use parent.Alert._crmContext to get back to the CRM window from inside an iframe
-        Alert._crmContext = window;
-        window.top.Alert = Alert;
-
         // The wrapper sits outside the form, so it may exist even if Alert.js is not initialised
-        var alertJsWrapper = Alert.$("#alertJs-wrapper");
+        var alertJsWrapper = $("#alertJs-wrapper", Alert._context);
         if (alertJsWrapper == null || alertJsWrapper.length == 0) {
             var alertJsHtml =
                 "<div id='alertJs-wrapper' class='alert-js-wrapper'>" +
                   "<link rel='stylesheet' href='" + baseUrl + "/WebResources/" + Alert._prefix + "/css/alert.css' />" +
                   "<div class='alert-js-background'></div>" +
                   "<div id='alertJs-dialog' class='alert-js-dialog'>" +
+                    "<div id='alertJs-closeWrapper' class='alert-js-close-wrapper'>" +
+                      "<div id='alertJs-close' class='alert-js-close' title='Cancel'></div>" +
+                    "</div>" +
                     "<div class='alert-js-RefreshDialog-Warning' id='alertJs-divWarning'>" +
                       "<table class='alert-js-table-wrapper' cellspacing='0' cellpadding='0'>" +
                         "<tr id='alertJs-errorRow'>" +
@@ -78,17 +66,14 @@ Alert.show = function (title, message, buttons, icon, width, height, baseUrl, pr
                       "</table>" +
                     "</div>" +
                     "<div class='alert-js-RefreshDialog-Footer' id='alertJs-tdDialogFooter'></div>" +
-                    "<div id='alertJs-closeWrapper' class='alert-js-close-wrapper'>" +
-                      "<div id='alertJs-close' class='alert-js-close' title='Cancel'></div>" +
-                    "</div>" +
                   "</div>" +
                 "</div>";
 
-            Alert.$("body").append(alertJsHtml);
+            $("body", Alert._context).append(alertJsHtml);
         }
 
         // Attach close event (messes up with jquery)
-        var closeButton = Alert.$("#alertJs-close");
+        var closeButton = $("#alertJs-close", Alert._context);
         if (closeButton && closeButton.length > 0) {
             closeButton[0].onclick = function () {
                 Alert.hide();
@@ -99,39 +84,30 @@ Alert.show = function (title, message, buttons, icon, width, height, baseUrl, pr
     }
 
     // Update the title and message
-    Alert.$("#alertJs-title").html(title);
-    Alert.$("#alertJs-message").html(message);
-
-    // Hide title if not specified
-    if (title == "") { Alert.$("#alertJs-title").hide(); }
-    else { Alert.$("#alertJs-title").show(); }
-
-    // Hide message if not specified
-    if (message == "") { Alert.$("#alertJs-message").hide(); }
-    else { Alert.$("#alertJs-message").show(); }
+    $("#alertJs-title", Alert._context).html(title);
+    $("#alertJs-message", Alert._context).html(message);
 
     // Add the icon
     if (icon && ["INFO", "WARNING", "ERROR", "SUCCESS", "QUESTION", "LOADING"].indexOf(icon) !== -1) {
         var imgType = icon == "ERROR" ? "crit" : icon == "WARNING" ? "warn" : icon == "INFO" ? "info" : icon == "SUCCESS" ? "tick" : icon == "QUESTION" ? "ques" : "load";
 
-        Alert.$("#alertJs-imageWrapper").show();
+        $("#alertJs-imageWrapper", Alert._context).removeClass("alert-js-hide");
 
         // Remove any existing image classes before adding the new one
-        Alert.$("#alertJs-image")
+        $("#alertJs-image", Alert._context)
             .removeClass("alert-js-image-crit alert-js-image-warn alert-js-image-info alert-js-image-tick alert-js-image-ques alert-js-image-load")
             .addClass("alert-js-image-" + imgType);;
     }
     else {
-        // Hide icon if not specified
-        Alert.$("#alertJs-imageWrapper").hide();
+        $("#alertJs-imageWrapper", Alert._context).addClass("alert-js-hide");
     }
 
     // Delete existing buttons
-    Alert.$("#alertJs-tdDialogFooter").empty();
+    $("#alertJs-tdDialogFooter", Alert._context).empty();
 
     // Create new buttons
     for (var i = 0; i < buttons.length; i++) {
-        var $button = Alert._jQuery("<button>", { tabindex: "1", type: "button" });
+        var $button = $("<button>", { tabindex: "1", type: "button" });
         $button.addClass("alert-js-RefreshDialog-Button");
 
         // Set focus to the button if explicitly specified, or if only one button
@@ -148,60 +124,49 @@ Alert.show = function (title, message, buttons, icon, width, height, baseUrl, pr
             });
         })(buttons[i].callback, buttons[i].preventClose);
 
-        Alert.$("#alertJs-tdDialogFooter").append($button);
+        $("#alertJs-tdDialogFooter", Alert._context).append($button);
     }
 
     if (buttons.length > 0) {
+        // Set focus to the button(s) if applicable
+        $(".alert-js-RefreshDialog-Button-focus", Alert._context).focus();
+
         // Show the buttons bar
-        Alert.$("#alertJs-divWarning").removeClass("alert-js-maxHeight");
-        Alert.$("#alertJs-tdDialogFooter").show();
+        $("#alertJs-divWarning", Alert._context).removeClass("alert-js-maxHeight");
+        $("#alertJs-tdDialogFooter", Alert._context).show();
     }
     else {
         // Hide the buttons bar
-        Alert.$("#alertJs-divWarning").addClass("alert-js-maxHeight");
-        Alert.$("#alertJs-tdDialogFooter").hide();
+        $("#alertJs-divWarning", Alert._context).addClass("alert-js-maxHeight");
+        $("#alertJs-tdDialogFooter", Alert._context).hide();
     }
 
     // Show or hide the manual cancel button
-    if (preventCancel) { Alert.$("#alertJs-closeWrapper").hide(); }
-    else { Alert.$("#alertJs-closeWrapper").show(); }
+    if (preventCancel) { $("#alertJs-closeWrapper", Alert._context).hide(); }
+    else { $("#alertJs-closeWrapper", Alert._context).show(); }
 
     // Makes the formatting nicer if the popup is huge (for displaying trace logs etc)
-    if (height > 250) { Alert.$(".alert-js-td").addClass("alert-js-td-top"); }
-    else { Alert.$(".alert-js-td").removeClass("alert-js-td-top"); }
+    if (height > 250) { $(".alert-js-td", Alert._context).addClass("alert-js-td-top"); }
+    else { $(".alert-js-td", Alert._context).removeClass("alert-js-td-top"); }
 
     // Set height/width of the alert
-    Alert.$("#alertJs-dialog").css("height", height).css("width", width).css("margin-top", height * -0.5).css("margin-left", width * -0.5);
-
-    // Set the height of the message body, to allow it to use the max space if buttons are hidden, or title is hidden, and allows scrollbar
-    Alert.$("#alertJs-message").css("max-height", Alert._calculateMessageHeight(height, padding, buttons.length, title));
-
-    // Set the padding of the light-box
-    Alert.$(".alert-js-RefreshDialog-Warning").css("left", padding).css("right", padding);
-    Alert.$(".alert-js-td").css("padding-top", padding).css("padding-bottom", padding);
+    $("#alertJs-dialog", Alert._context).css("height", height).css("width", width).css("margin-top", height * -0.5).css("margin-left", width * -0.5);
+    $("#alertJs-message", Alert._context).css("max-height", height - 96 - (buttons.length > 0 ? 44 : 0));
 
     // Show the alert wrapper
-    Alert.$("#alertJs-wrapper").show();
-
-    // Set focus to the button(s) if applicable
-    Alert.$(".alert-js-RefreshDialog-Button-focus").focus();
+    $("#alertJs-wrapper", Alert._context).show();
 }
 
 // Hide the alert manually without performing any callbacks
 Alert.hide = function () {
     if (Alert._initialised) {
-        Alert.$("#alertJs-wrapper").hide();
+        $("#alertJs-wrapper", Alert._context).hide();
     }
-}
-
-// Calculates the height of the sub-heading/message based on other variables
-Alert._calculateMessageHeight = function (dialogHeight, dialogPadding, buttonLength, title) {
-    return dialogHeight - (dialogPadding * 2) - (buttonLength > 0 ? 44 : 0) - (title != null && title != "" ? 32 : 0);
 }
 
 // Internal button click event
 Alert._buttonClicked = function (callback, preventClose) {
-    Alert.$(".alert-js-RefreshDialog-Button").prop("disabled", true);
+    $(".alert-js-RefreshDialog-Button", Alert._context).prop("disabled", true);
 
     try {
         // Unless specified, close the alert after executing the callback
@@ -217,7 +182,7 @@ Alert._buttonClicked = function (callback, preventClose) {
         alert(e);
     }
 
-    Alert.$(".alert-js-RefreshDialog-Button").prop("disabled", false);
+    $(".alert-js-RefreshDialog-Button", Alert._context).prop("disabled", false);
 }
 
 // Encode the Title or Message to display xml tags, e.g. from a plugin error trace
@@ -230,70 +195,6 @@ Alert.htmlEncode = function (text) {
 
 Alert.showLoading = function (url) {
     Alert.show("Loading...", null, [], "LOADING", 230, 115, url, true);
-}
-
-Alert.showWebResource = function (webResourceName, width, height, title, buttons, baseUrl, preventCancel, padding) {
-    baseUrl = baseUrl || Xrm.Page.context.getClientUrl();
-    buttons = buttons || []; // No buttons displayed if null, rather than 'OK'
-
-    var iframeUrl = baseUrl + "/webresources/" + webResourceName;
-
-    Alert.showIFrame(iframeUrl, width, height, title, buttons, baseUrl, preventCancel, padding);
-}
-
-Alert.showDialogProcess = function (dialogId, entityName, recordId, callback, width, height, baseUrl) {
-    baseUrl = baseUrl || Xrm.Page.context.getClientUrl();
-
-    var dialogUrl = baseUrl + "/cs/dialog/rundialog.aspx?DialogId=%7b" + dialogId + "%7d&EntityName=" + entityName + "&ObjectId=" + recordId;
-
-    Alert.showIFrame(dialogUrl, width, height, null, null, baseUrl);
-
-    // Handle the callback and close actions (otherwise it tries to close the whole form, rather than the popup)
-    var $frame = Alert.$("#alertJs-iFrame");
-    $frame.load(function () {
-        try {
-            // Override the CRM closeWindow function (unsupported)
-            var frameDoc = $frame[0].contentWindow;
-            frameDoc.closeWindow = function () {
-                // Fire the callback and close
-                if (callback) { callback(); }
-
-                Alert.hide();
-            }
-        } catch (e) { }
-    });
-}
-
-Alert.showIFrame = function (iframeUrl, width, height, title, buttons, baseUrl, preventCancel, padding) {
-    width = width || 800;
-    height = height || 600;
-    buttons = buttons || []; // No buttons displayed if null, rather than 'OK'
-    if (padding === undefined || padding === null) { padding = 0; }
-
-    var iframeHtml = "<iframe id='alertJs-iFrame' class='alert-js-iframe' src='" + Alert.htmlEncode(iframeUrl) + "'></iframe>";
-
-    Alert.show(title, iframeHtml, buttons, null, width, height, baseUrl, preventCancel, padding);
-
-    // Set a fixed height on the iframe (minus 3 for some reason) - it doesn't like relative heights, i.e. calc(100% - 3)
-    Alert.$("#alertJs-iFrame").css("height", Alert._calculateMessageHeight(height, padding, buttons.length, title) - 3);
-}
-
-// Use the returned iframe context with jQuery to get data from the iframe, i.e. Alert.$("#something", Alert.getIFrameContext().document);
-Alert.getIFrameWindow = function () {
-    var iframeContext = null;
-
-    if (Alert._initialised) {
-        var iframe = Alert.$("#alertJs-iFrame");
-
-        if (iframe.length > 0) {
-            try {
-                iframeContext = iframe[0].contentWindow;
-            }
-            catch (e) { }
-        }
-    }
-
-    return iframeContext;
 }
 
 // Helper to build the buttons
